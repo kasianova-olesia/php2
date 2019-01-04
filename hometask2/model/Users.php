cf9902b11d7fa91aea4f9f3f19c70cca9edfca72
kasianova olesia <kas.olesia@gmail.com>
2019-01-04 21:00:06 +0300

    Create Users.php
    ДЗ2

---
+++ hometask2/model/Users.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\model;
+
+class Users extends Model
+{
+    public $id;
+    public $login;
+    public $pass;
+    public function getTableName()
+    {
+        return "users";
+    }
+}
