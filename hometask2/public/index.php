543149c35e49cca45e13de9b37d2ad94a4268f6b
kasianova olesia <kas.olesia@gmail.com>
2019-01-04 21:00:13 +0300

    Create index.php
    ДЗ2

---
+++ hometask2/public/index.php
@@ -0,0 +1,12 @@
+<?php
+use app\engine\Autoload;
+use app\model\Products;
+use app\engine\Db;
+
+include "../engine/Autoload.php";
+
+spl_autoload_register([new Autoload(), 'loadClass']);
+
+$product = new Products(new Db());
+
+var_dump($product);
