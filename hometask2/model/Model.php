7fd07f51ed2845be149aa00fa2b165394a4c4d85
kasianova olesia <kas.olesia@gmail.com>
2019-01-04 20:59:58 +0300

    Create Model.php
    ДЗ2

---
+++ hometask2/model/Model.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\model;
+use app\engine\Db;
+
+abstract class Model
+{
+    protected $db;
+    public function __construct(Db $db)
+    {
+        $this->db = $db;
+    }
+    public function getOne($id) {
+        $tableName = $this->getTableName();
+        $sql = "SELECT * FROM {$tableName} WHERE id = {$id}";
+        return $this->db->queryOne($sql);
+    }
+    public function getAll() {
+        $tableName = $this->getTableName();
+        $sql = "SELECT * FROM {$tableName}";
+        return $this->db->queryAll($sql);
+    }
+
+    abstract public function getTableName();
+
+}
