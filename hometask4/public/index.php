345cfaf7e875cbb3a12a3490fc86844d6501dcb5
kasianova olesia <kas.olesia@gmail.com>
2019-01-05 15:34:47 +0300

    Create index.php
    ДЗ-4

---
+++ hometask4/public/index.php
@@ -0,0 +1,23 @@
+<?php
+include "../config/main.php";
+include "../services/Autoloader.php";
+
+spl_autoload_register([new \app\services\Autoloader(), 'loadClass']);
+
+
+// index.php?c=product&a=card - Вывод всех карточек товаров
+// index.php?c=product&a=card&id=1 - Вывод конкретной карточки товара
+/*Авторизация*/
+// index.php?c=auth&a=login&login=admin&pass=123
+
+// Controller - если не пришел, то открываем "Auth"
+$controllerName = $_GET['c'] ?: "Auth";
+// Action
+$actionName = $_GET['a']  ?: "Login";;
+
+// Формируем название класса из полученных значений
+$controllerClass = CONTROLLERS_NAMESPACE . ucfirst($controllerName) . "Controller";
+// Создаем экземпляр контроллера
+$controller = new $controllerClass();
+
+$controller->run($actionName);
