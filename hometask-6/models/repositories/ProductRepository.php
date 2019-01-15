7b9c266b09cd3ed24811270da7f074a213c5375a
kasianova olesia <kas.olesia@gmail.com>
2019-01-15 13:14:13 +0300

    Create ProductRepository.php
    +

---
+++ hometask-6/models/repositories/ProductRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\models\repositories;
+
+use app\models\Product;
+
+class ProductRepository extends Repository
+{
+
+    /**
+     * ProductRepository constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->tableName = "product";
+        $this->entityClass = Product::class;
+    }
+} 
