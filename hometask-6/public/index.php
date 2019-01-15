dc61cbd283d50f8d3a62f694931048507c61bdc5
kasianova olesia <kas.olesia@gmail.com>
2019-01-15 13:14:32 +0300

    Create index.php
    +

---
+++ hometask-6/public/index.php
@@ -0,0 +1,12 @@
+<?php
+include "../config/main.php";
+include "../services/Autoloader.php";
+include "../vendor/autoload.php";
+
+spl_autoload_register([new app\services\Autoloader(), 'loadClass']);
+
+(new \app\controllers\FrontController())->runAction();
+
+// Тестирование
+// http://geekbrains/product/card?id=4
+// http://geekbrains/service/error404 
