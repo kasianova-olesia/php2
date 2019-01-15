e040c895c5481b3690d9c42731604c1f5786ae72
kasianova olesia <kas.olesia@gmail.com>
2019-01-15 13:13:57 +0300

    Create Autoloader.php
    +

---
+++ hometask-6/services/Autoloader.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\services;
+
+class AutoloaderNotMatchException extends \Exception{}
+
+class Autoloader
+{
+    private $fileExtension = ".php";
+
+    function loadClass($className)
+    {
+        $className = str_replace("app\\", ROOT_DIR, $className);
+        $className = str_replace("\\", "/", $className) . $this->fileExtension;
+        if (file_exists($className)) {
+            include $className;
+        } else {
+            throw new AutoloaderNotMatchException("Файл с классом не найден!");
+        }
+    }
+} 
